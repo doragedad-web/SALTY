5a089470e893dcddbd3d5684d06ea02fd276ab7e
doragedad-web <doragedad@gmail.com>
2026-06-06 21:01:49 +0530

    Initialize Express webhook server in Index.js

---
+++ Index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/webhook", (req, res) => {
+    console.log("🔥 Webhook received:");
+    console.log(req.body);
+
+    res.sendStatus(200);
+});
+
+app.get("/", (req, res) => {
+    res.send("Webhook server is alive");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log("Server running on port", port);
+});
