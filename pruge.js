8a5d7030691cfc33f88693accd1d8833f2328755
doragedad-web <doragedad@gmail.com>
2026-06-09 23:28:09 +0530

    Update pruge.js

--- pruge.js
+++ pruge.js
@@ -1,17 +1,29 @@
-const { PermissionFlagsBits } = require("discord.js");
+import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
+import { withErrorHandling } from '../../utils/errorHandler.js';
+import { InteractionHelper } from '../../utils/interactionHelper.js';
 
-module.exports = {
-    name: "pruge30",
-    description: "Deletes the last 30 messages.",
-    
-    async execute(message) {
-        if (!message.member.permissions.has(PermissionFlagsBits.ManageMessages)) {
-            return message.reply("You need Manage Messages permission.");
-        }
+export default {
+    data: new SlashCommandBuilder()
+        .setName('purge')
+        .setDescription('Delete messages from the current channel')
+        .addIntegerOption(option =>
+            option
+                .setName('amount')
+                .setDescription('Number of messages to delete (1-100)')
+                .setRequired(true)
+                .setMinValue(1)
+                .setMaxValue(100)
+        )
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
 
-        const messages = await message.channel.bulkDelete(30, true);
+    execute: withErrorHandling(async (interaction) => {
+        const amount = interaction.options.getInteger('amount');
 
-        message.channel.send(`🗑️ Deleted ${messages.size} messages.`)
-            .then(msg => setTimeout(() => msg.delete().catch(() => {}), 3000));
-    }
+        const deleted = await interaction.channel.bulkDelete(amount, true);
+
+        await InteractionHelper.safeReply(interaction, {
+            content: `🗑️ Deleted ${deleted.size} messages.`,
+            ephemeral: true
+        });
+    }, { command: 'purge' })
 };
