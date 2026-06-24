fd2550f7609497a680ed823ce062659f91427379
doragedad-web <doragedad@gmail.com>
2026-06-24 13:34:24 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { Client, Collection, GatewayIntentBits } from 'discord.js';
 import { REST } from '@discordjs/rest';
 import express from 'express';
 import cron from 'node-cron';
+import { GoogleGenAI } from '@google/genai';
 
 import config from './config/application.js';
 import { initializeDatabase } from './utils/database.js';
